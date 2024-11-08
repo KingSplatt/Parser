7f9835d97ed85a5fabc56087f090a18ae52df31a
KingSplatt <malp18774@gmail.com>
2024-11-08 11:58:47 -0800

    avanza

--- Compilador.java
+++ Compilador.java
@@ -3,5 +3,6 @@ public class Compilador {
     public static void main(String[] args) {
         String codigo = "print 5 + 5";
         Parser parser = new Parser(codigo);
+        parser.P();
     }
 }
