481397e74aa3cfe0a9de9744ed5a6128ddc46ecd
KingSplatt <malp18774@gmail.com>
2025-01-29 19:16:32 -0700

    Comienzo de la parte grafica

--- Compilador.java
+++ Compilador.java
@@ -1,6 +1,24 @@
-public class Compilador {
+import javax.swing.*;
+import java.awt.*;
+
+public class Compilador extends JPanel {
+    public Compilador() {
+        setBackground(Color.WHITE);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        g.drawString("Hola Mundo", 100, 100);
+    }
 
     public static void main(String[] args) throws Exception {
+        JFrame ventana = new JFrame("Compilador");
+        Compilador compilador = new Compilador();
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setSize(300, 300);
+        ventana.add(compilador);
+        ventana.setVisible(true);
         // String codigo = "a83 int ; b32 string ; while a3 + a23 do while hola do while
         // pepe do print s + a84";
         // String codigo = "while a63 do while ba28 + a43 do print hola23 + a42";
@@ -8,8 +26,9 @@ public class Compilador {
         // String codigo = "while a63 do while ba28 + a43 do print hola23 + while";
         // String codigo = "print a3 + b82 ; print b8 ;";
         // String codigo = "";
-        String codigo = "id = ha + as2";
-        Parser parser = new Parser(codigo);
-        parser.P();
+
+        // String codigo = "id = ha + as2";
+        // Parser parser = new Parser(codigo);
+        // parser.P();
     }
 }
